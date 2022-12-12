1552f6a28027e8ffde5d45e051b8c3ccd83f2fd1
lovretrogrlic <39429420+lt3528@users.noreply.github.com>
2022-12-12 14:42:59 +0100

    Update zadatak01.c

--- zadatak01.c
+++ zadatak01.c
@@ -1,1 +1,10 @@
 // Napravite program koji učitava n cijelih brojeva, a zatim provjerava i ispisuje koliko je brojeva od učitanih neparno.
+
+#include<stdio.h>
+
+int main()
+{
+  
+  
+  return 0;
+}
